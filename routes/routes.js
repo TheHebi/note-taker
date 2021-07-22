41af09c665bd10ca218dbd3e0a4c48e74057c761
TheHebi <nturcotte8@live.com>
2021-07-21 19:13:13 -0700

    added route for adding notes

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const notes = require('express').Router();
+const api = require('../db/notes.json')
 const { readFromFile, readAndAppend } = require("../helpers/fsUtils");
 // const app = express();
 
@@ -12,5 +13,30 @@ notes.get("/", (req, res) =>
 notes.get("/notes", (req, res) =>
   res.sendFile(path.join(__dirname, "../public/notes.html"))
 );
+
+ // API GET Request
+ notes.get('/api/notes', (req, res) => {
+    res.json(api);
+});
+
+// POST route for notes
+notes.post("/api/notes", (req, res) => {
+  console.info(`${req.method} request received to add a note`);
+  console.log(req.body);
+
+  const { title, text } = req.body;
+
+  if (req.body) {
+    const newNote = {
+      title,
+      text
+    };
+
+    readAndAppend(newNote, "./db/notes.json");
+    res.json(`note added successfully 🚀`);
+  } else {
+    res.error("Error in adding tip");
+  }
+});
 
 module.exports = notes
